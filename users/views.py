5d46ae4593570571ed077301fbd9be3bb35e7282
lavrik <businessphot@gmail.com>
2019-01-30 22:46:08 -1000

    Added list of last guest

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import SimpleGuest
 
 # Create your views here.
 def guest_list(request):
-    return render(request, 'users/guest_list.html', {})
+    simple_guest = SimpleGuest.objects.order_by('-simple_guest_id')[:5]
+    return render(request, 'users/guest_list.html', {'simple_guest':simple_guest})
